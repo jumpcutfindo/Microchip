960cb6f1740a335db3c3fa8395f8da872404442a
jumpcutfindo <danielhoe97@gmail.com>
2022-10-21 01:34:47 +0800

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Microchip
+
+Microchip is a mod that allows the player to keep track of mobs across their world.
---
+++ src/main/java/com/jumpcutfindo/microchip/MicrochipMod.java
@@ -0,0 +1,15 @@
+package com.jumpcutfindo.microchip;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MicrochipMod implements ModInitializer {
+	public static final String MOD_ID = "microchip";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
