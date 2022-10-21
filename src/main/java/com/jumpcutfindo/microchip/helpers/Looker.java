c870f1bce445b1bf97eeb2a6ac5396578ec1d9a1
jumpcutfindo <danielhoe97@gmail.com>
2022-10-21 17:31:04 +0800

    Create Looker to help find looked at entity

---
+++ src/main/java/com/jumpcutfindo/microchip/helpers/Looker.java
@@ -0,0 +1,41 @@
+package com.jumpcutfindo.microchip.helpers;
+
+import java.util.List;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public class Looker {
+    public static LivingEntity getLookingAt(World world, PlayerEntity player) {
+        BlockPos playerPos = player.getBlockPos();
+        if (world == null) return null;
+        List<LivingEntity> entities = world
+                .getEntitiesByClass(LivingEntity.class, new Box(playerPos).expand(15.0), (entity) -> {
+                    return !entity.getUuid().equals(player.getUuid());
+                });
+
+        for (LivingEntity entity : entities) {
+            Vec3d vec3d = player.getRotationVec(1.0F).normalize();
+            Vec3d vec3d2 = new Vec3d(
+                    entity.getX() - player.getX(),
+                    entity.getEyeY() - player.getEyeY(),
+                    entity.getZ() - player.getZ()
+            );
+            double d = vec3d2.length();
+            vec3d2 = vec3d2.normalize();
+            double e = vec3d.dotProduct(vec3d2);
+            boolean isLooking = e > 1.0 - 0.025 / d && player.canSee(entity);
+
+            if (isLooking) {
+                return entity;
+            }
+        }
+
+        return null;
+    }
+
+}
