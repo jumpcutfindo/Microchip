9b6179775aa793b935fec9a3b3a45e3427dcd615
jumpcutfindo <danielhoe97@gmail.com>
2022-10-23 00:19:46 +0800

    Change underlying list of chips to set

--- src/main/java/com/jumpcutfindo/microchip/data/MicrochipGroup.java
+++ src/main/java/com/jumpcutfindo/microchip/data/MicrochipGroup.java
@@ -1,8 +1,6 @@
 package com.jumpcutfindo.microchip.data;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import org.slf4j.Logger;
 
@@ -12,12 +10,12 @@ public class MicrochipGroup {
     public static Logger LOGGER = MicrochipMod.LOGGER;
     private UUID id;
     private String displayName;
-    private List<Microchip> microchips;
+    private Set<Microchip> microchips;
 
     public MicrochipGroup(String displayName) {
         this.id = UUID.randomUUID();
         this.displayName = displayName;
-        this.microchips = new ArrayList<>();
+        this.microchips = new HashSet<>();
     }
 
     public UUID getId() {
@@ -29,12 +27,12 @@ public class MicrochipGroup {
     }
 
     public boolean add(Microchip microchip) {
-        LOGGER.info(String.format("Adding Microchip(%s) to group %s", microchip, displayName));
+        LOGGER.info(String.format("Adding Microchip(%s) to group(%s)", microchip, displayName));
         return this.microchips.add(microchip);
     }
 
     public boolean remove(Microchip microchip) {
-        LOGGER.info(String.format("Removing Microchip(%s) to group %s", microchip, displayName));
+        LOGGER.info(String.format("Removing Microchip(%s) from group(%s)", microchip, displayName));
         return this.microchips.remove(microchip);
     }
 
