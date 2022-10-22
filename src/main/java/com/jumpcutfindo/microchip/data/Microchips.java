bcc5538341da0f04d476c751ac257f4d727834f3
jumpcutfindo <danielhoe97@gmail.com>
2022-10-22 13:31:11 +0800

    Add component class to Microchips

--- src/main/java/com/jumpcutfindo/microchip/data/Microchips.java
+++ src/main/java/com/jumpcutfindo/microchip/data/Microchips.java
@@ -1,10 +1,17 @@
 package com.jumpcutfindo.microchip.data;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class Microchips {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import dev.onyxstudios.cca.api.v3.component.Component;
+import net.minecraft.nbt.NbtCompound;
+
+public class Microchips implements Component {
     private List<MicrochipGroup> groups;
 
     public Microchips() {
@@ -18,5 +25,24 @@ public class Microchips {
 
     public boolean deleteGroup(UUID id) {
         return groups.removeIf(group -> group.getId().equals(id));
+    }
+
+    @Override
+    public void readFromNbt(NbtCompound tag) {
+        Gson gson = new Gson();
+
+        NbtCompound cpd = tag.getCompound("microchips");
+        Type groupsType = new TypeToken<List<MicrochipGroup>>(){}.getType();
+        this.groups = gson.fromJson(cpd.getString("groups"), groupsType);
+    }
+
+    @Override
+    public void writeToNbt(NbtCompound tag) {
+        Gson gson = new Gson();
+
+        NbtCompound cpd = new NbtCompound();
+        cpd.putString("groups", gson.toJson(groups));
+
+        tag.put("microchips", cpd);
     }
 }
