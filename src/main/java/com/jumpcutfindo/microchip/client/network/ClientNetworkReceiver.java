af74c8a26de77fef1b0d9b31b7c77fb38b8f0465
jumpcutfindo <danielhoe97@gmail.com>
2023-02-23 22:34:57 +0800

    Fix data not going into info window

--- src/main/java/com/jumpcutfindo/microchip/client/network/ClientNetworkReceiver.java
+++ src/main/java/com/jumpcutfindo/microchip/client/network/ClientNetworkReceiver.java
@@ -1,6 +1,7 @@
 package com.jumpcutfindo.microchip.client.network;
 
 import com.jumpcutfindo.microchip.constants.NetworkConstants;
+import com.jumpcutfindo.microchip.screen.MicrochipScreen;
 import com.jumpcutfindo.microchip.screen.MicrochipsMenuScreen;
 import com.jumpcutfindo.microchip.screen.window.MicrochipInfoWindow;
 import net.fabricmc.api.ClientModInitializer;
@@ -18,11 +19,10 @@ public class ClientNetworkReceiver implements ClientModInitializer {
 
     public static void onEntityDataResponse() {
         ClientPlayNetworking.registerGlobalReceiver(NetworkConstants.PACKET_REQUEST_ENTITY_DATA_ID, (client, handler, buf, responseSender) -> {
-
             Collection<StatusEffectInstance> entityStatuses = buf.readCollection((size) -> new ArrayList<>(), (packetByteBuf -> StatusEffectInstance.fromNbt(packetByteBuf.readNbt())));
             int breedingAge = buf.readInt();
 
-            if (client.currentScreen instanceof MicrochipsMenuScreen screen && screen.getActiveWindow() instanceof MicrochipInfoWindow infoWindow) {
+            if (client.currentScreen instanceof MicrochipScreen screen && screen.getActiveWindow() instanceof MicrochipInfoWindow infoWindow) {
                 infoWindow.setEntityStatuses(entityStatuses);
                 infoWindow.setBreedingAge(breedingAge);
             }
