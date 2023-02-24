5b7b1e2916a6642e7d19e837f9b3685c20333132
jumpcutfindo <danielhoe97@gmail.com>
2023-02-24 17:52:16 +0800

    Add retrieval of item list from server

--- src/main/java/com/jumpcutfindo/microchip/server/network/ServerNetworkSender.java
+++ src/main/java/com/jumpcutfindo/microchip/server/network/ServerNetworkSender.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
 
@@ -31,6 +32,17 @@ public class ServerNetworkSender {
         } else {
             buffer.writeInt(0);
         }
+
+        // Entity inventory
+        NbtCompound entityNbt = entity.writeNbt(new NbtCompound());
+        NbtCompound inventoryNbt = new NbtCompound();
+        NbtList itemListNbt = new NbtList();
+        
+        if (entityNbt.contains("Inventory")) itemListNbt = entityNbt.getList("Inventory", 10);
+        else if (entityNbt.contains("Items")) itemListNbt = entityNbt.getList("Items", 10);
+
+        inventoryNbt.put("Inventory", itemListNbt);
+        buffer.writeNbt(inventoryNbt);
 
         ServerPlayNetworking.send(player, NetworkConstants.PACKET_REQUEST_ENTITY_DATA_ID, buffer);
     }
