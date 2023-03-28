4bcbdff60a70c763423dd83c702f41e3e5ba4eaf
jumpcutfindo <danielhoe97@gmail.com>
2023-03-28 21:38:43 +0800

    Reorganize type retrievers

--- src/main/java/com/jumpcutfindo/microchip/data/EntityNamer.java
+++ src/main/java/com/jumpcutfindo/microchip/data/EntityNamer.java
@@ -16,37 +16,11 @@ public class EntityNamer {
      */
     public static final Map<Class<? extends LivingEntity>, Function<LivingEntity, String>> ENTITY_TYPE_NAMES =
             ImmutableMap.<Class<? extends LivingEntity>, Function<LivingEntity, String>>builder()
-                    .put(LivingEntity.class, (entity) -> entity.getType().getName().getString())
-                    .put(AxolotlEntity.class, (entity -> {
-                        AxolotlEntity.Variant variant = ((AxolotlEntity) entity).getVariant();
-                        return nameWithVariant("Axolotl", variant.getName());
-                    }))
-                    .put(CatEntity.class, (entity -> {
-                        int catType = ((CatEntity) entity).getCatType();
-                        switch (catType) {
-                            case 0: return "Tabby";
-                            case 1: return "Black";
-                            case 2: return "Red";
-                            case 3: return "Siamese";
-                            case 4: return "British Shorthair";
-                            case 5: return "Calico";
-                            case 6: return "Persian";
-                            case 7: return "Ragdoll";
-                            case 8: return "White";
-                            case 9: return "Jellie";
-                            case 10: return "All Black";
-                            default: return "Cat";
-                        }
-                    }))
-                    .put(HorseEntity.class, (entity) -> {
-                        HorseColor color = ((HorseEntity) entity).getColor();
-                        return nameWithVariant("Horse", color.name());
-                    })
-                    .put(VillagerEntity.class, (entity -> {
-                        VillagerProfession profession = ((VillagerEntity) entity).getVillagerData().getProfession();
-                        if (profession == VillagerProfession.NONE) return "Villager";
-                        else return profession.toString().substring(0, 1).toUpperCase() + profession.toString().substring(1);
-                    }))
+                    .put(LivingEntity.class, EntityNamer::getDefaultType)
+                    .put(AxolotlEntity.class, EntityNamer::getAxolotlType)
+                    .put(CatEntity.class, EntityNamer::getCatType)
+                    .put(HorseEntity.class, EntityNamer::getHorseType)
+                    .put(VillagerEntity.class, EntityNamer::getVillagerType)
                     .build();
 
     public static String getDisplayName(LivingEntity entity) {
@@ -56,6 +30,44 @@ public class EntityNamer {
     public static String getTypeName(LivingEntity entity) {
         if (ENTITY_TYPE_NAMES.containsKey(entity.getClass())) return ENTITY_TYPE_NAMES.get(entity.getClass()).apply(entity);
         else return ENTITY_TYPE_NAMES.get(LivingEntity.class).apply(entity);
+    }
+
+    private static String getDefaultType(LivingEntity entity) {
+        return entity.getType().getName().getString();
+    }
+
+    private static String getAxolotlType(LivingEntity entity) {
+        AxolotlEntity.Variant variant = ((AxolotlEntity) entity).getVariant();
+        return nameWithVariant("Axolotl", variant.getName());
+    }
+
+    private static String getCatType(LivingEntity entity) {{
+        int catType = ((CatEntity) entity).getCatType();
+        switch (catType) {
+            case 0: return "Tabby";
+            case 1: return "Black";
+            case 2: return "Red";
+            case 3: return "Siamese";
+            case 4: return "British Shorthair";
+            case 5: return "Calico";
+            case 6: return "Persian";
+            case 7: return "Ragdoll";
+            case 8: return "White";
+            case 9: return "Jellie";
+            case 10: return "All Black";
+            default: return "Cat";
+        }
+    }}
+
+    private static String getHorseType(LivingEntity entity) {
+        HorseColor color = ((HorseEntity) entity).getColor();
+        return nameWithVariant("Horse", color.name());
+    }
+
+    private static String getVillagerType(LivingEntity entity) {
+        VillagerProfession profession = ((VillagerEntity) entity).getVillagerData().getProfession();
+        if (profession == VillagerProfession.NONE) return "Villager";
+        else return profession.toString().substring(0, 1).toUpperCase() + profession.toString().substring(1);
     }
 
     private static String nameWithVariant(String name, String variant) {
