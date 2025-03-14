2b6cbf4cc290ebe15b8a6a1832f22356fbb2fd0f
jumpcutfindo <danielhoe97@gmail.com>
2025-03-14 16:46:43 +0800

    Update minimum dependencies for mod

--- src/main/java/com/jumpcutfindo/microchip/client/network/ClientNetworkReceiver.java
+++ src/main/java/com/jumpcutfindo/microchip/client/network/ClientNetworkReceiver.java
@@ -1,20 +1,10 @@
 package com.jumpcutfindo.microchip.client.network;
 
-import com.jumpcutfindo.microchip.constants.NetworkConstants;
 import com.jumpcutfindo.microchip.packets.EntityDataPacket;
 import com.jumpcutfindo.microchip.screen.MicrochipScreen;
 import com.jumpcutfindo.microchip.screen.window.MicrochipInfoWindow;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 public class ClientNetworkReceiver implements ClientModInitializer {
     @Override
